a1d7e8bb9191e1e672db01ce35a5a78c4ebeec6a
enteraz316 <143341449+enteraz316@users.noreply.github.com>
2024-11-30 10:48:52 +0300

    Update test_cases.cpp

--- String/test_cases.cpp
+++ String/test_cases.cpp
@@ -252,7 +252,7 @@ void test_cases::back_front() {
 void test_cases::find_rfind() {
     String string("test string");
 
-    ASSERT(string.find("t") == 0, "find_rfind", "string.find("t) == 0");
+    ASSERT(string.find("t") == 0, "find_rfind", "string.find("t") == 0");
     ASSERT(string.rfind("t") == 6, "find_rfind", "string.rfind("t") == 6");
 
     ulong inf = string.size(); //for std::string  string.size() -> -1
